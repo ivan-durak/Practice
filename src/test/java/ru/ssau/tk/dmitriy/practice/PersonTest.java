f4cc4339cd08912911068cb003767c905e5035e6
Ostapyuk <dm.ostapyuk@yandex.ru>
2021-09-19 19:52:49 +0400

    11-е упражение выполнено согласно заданию, добавлены неработающие методы для демонстрации в случае проверки до задания значения, добавлены тесты для конструкторов(в рамках задания)

--- src/test/java/ru/ssau/tk/dmitriy/practice/PersonTest.java
+++ src/test/java/ru/ssau/tk/dmitriy/practice/PersonTest.java
@@ -6,24 +6,90 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class PersonTest {
+    //работающие методы
     @Test
-    public void testOfSet_Getters() {
-        String firstName1 = "Потап", firstName2 = "Игнат";
-        String lastName1 = "Капитулин", lastName2 = "Мумиев";
-        int firstPassportId = 456327291, secondPassportId = 120385472;
-        Person firstPeople = new Person();
-        Person secondPeople = new Person();
-        firstPeople.setFirstName(firstName1);
-        firstPeople.setLastName(lastName1);
-        firstPeople.setPassportId(firstPassportId);
-        secondPeople.setFirstName(firstName2);
-        secondPeople.setLastName(lastName2);
-        secondPeople.setPassportId(secondPassportId);
-        Assert.assertEquals(firstPeople.getFirstName(),firstName1);
-        Assert.assertEquals(firstPeople.getLastName(),lastName1);
-        Assert.assertEquals(firstPeople.getPassportId(),firstPassportId);
-        Assert.assertEquals(secondPeople.getFirstName(),firstName2);
-        Assert.assertEquals(secondPeople.getLastName(),lastName2);
-        Assert.assertEquals(secondPeople.getPassportId(),secondPassportId);
+    public void testOfFieldFirstName() { //работающий метод
+        String testValue = null; //для введения вместо null:Потап, Игнат
+        Person human = new Person();
+        human.setFirstName(testValue);
+        Assert.assertEquals(human.getFirstName(), testValue);
+    }
+
+    @Test
+    public void testOfFieldSecondName() { //работающий метод
+        String testValue = null; //для введения вместо null:Капитулин, Мумиев
+        Person human = new Person();
+        human.setLastName(testValue);
+        Assert.assertEquals(human.getLastName(), testValue);
+    }
+
+    @Test
+    public void testOfFieldPassportId() { //работающий метод
+        int testValue = 456327291; //120385472
+        Person human = new Person();
+        human.setPassportId(testValue);
+        Assert.assertEquals(human.getPassportId(), testValue);
+    }
+
+    //неработающие методы
+    @Test
+    public void testOfFieldFirstName2() { //не будет выполнен,т.к. вызов проверки до установки поля
+        String testValue = null;
+        Person human = new Person();
+        Assert.assertEquals(human.getFirstName(), testValue);
+        human.setFirstName(testValue);
+    }
+
+    @Test
+    public void testOfFieldSecondName2() { //не будет выполнен,т.к. вызов проверки до установки поля
+        String testValue = null;
+        Person human = new Person();
+        Assert.assertEquals(human.getLastName(), testValue);
+        human.setLastName(testValue);
+    }
+
+    @Test
+    public void testOfFieldPassportId2() { //не будет выполнен,т.к. вызов проверки до установки поля
+        int testValue = 456327291;
+        Person human = new Person();
+        Assert.assertEquals(human.getPassportId(), testValue);
+        human.setPassportId(testValue);
+    }
+
+    //тесты конструкторов
+    @Test
+    public void testOfFirstConstructor() { //Конструктор без параметров
+        Person testObject = new Person();
+        Assert.assertEquals(testObject.getFirstName(), ""); ////в оригинальном кострукторе так
+        Assert.assertEquals(testObject.getLastName(), "");
+        Assert.assertEquals(testObject.getPassportId(), 0);
+    }
+
+    @Test
+    public void testOfSecondConstructor() { //Конструктор с двумя параметрами:имя и фамилия
+        String testFirstName = "Арарат", testLastName = "Манекян";
+        Person testObject = new Person(testFirstName, testLastName);
+        Assert.assertEquals(testObject.getFirstName(), testFirstName);
+        Assert.assertEquals(testObject.getLastName(), testLastName);
+        Assert.assertEquals(testObject.getPassportId(), 0);
+    }
+
+    @Test
+    public void testOfThirdConstructor() { //Конструктор с одним параметром:паспорт
+        int testValue = 237451982;
+        Person testObject = new Person(testValue);
+        Assert.assertEquals(testObject.getFirstName(), ""); //в оригинальном кострукторе так
+        Assert.assertEquals(testObject.getLastName(), "");
+        Assert.assertEquals(testObject.getPassportId(), testValue);
+    }
+
+    @Test
+    public void testOfFourthConstructor() { //Конструктор с тремя параметрами
+        String testFirstName = "Аполон", testLastName = "Вяземский";
+        int testValue = 109428394;
+        Person testObject = new Person(testFirstName, testLastName, testValue);
+        Assert.assertEquals(testObject.getFirstName(), testFirstName);
+        Assert.assertEquals(testObject.getLastName(), testLastName);
+        Assert.assertEquals(testObject.getPassportId(), testValue);
     }
 }
