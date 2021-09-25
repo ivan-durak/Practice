21f215509fcffd65320bf36c883a6e70d6b8277b
Ostapyuk <dm.ostapyuk@yandex.ru>
2021-09-25 15:40:58 +0400

    В класс Person добавлено новое поле, доступ к которому осуществляется с помощью геттеров и сеттеров, также добавлено 2 новых конструктора для задания поля gender, дальше будет расширен класс-тест для класса Person

--- src/main/java/ru/ssau/tk/dmitriy/practice/Person.java
+++ src/main/java/ru/ssau/tk/dmitriy/practice/Person.java
@@ -4,27 +4,8 @@ public class Person {
     private String firstName;
     private String lastName;
     private int passportId;
+    private Gender gender;
 
-    public static void main(String[] args) {
-        String firstName1 = "Pavel", firstName2 = "Mihail", firstName3 = "Egor";
-        String lastName1 = "Ovchinkin", lastName2 = "Ortohov", lastName3 = "Futonasov";
-        int passportId1 = 349023185, passportId2 = 674523489, passportId3 = 239012965;
-        Person firstPeople = new Person();
-        Person secondPeople = new Person(firstName1, lastName1);
-        Person thirdPeople = new Person(passportId2);
-        Person fourthPeople = new Person(firstName3, lastName3, passportId3);
-        secondPeople.setPassportId(passportId1);
-        thirdPeople.setFirstName(firstName2);
-        thirdPeople.setLastName(lastName2);
-        System.out.println("Первый Человек- это "+firstPeople.getFirstName()+" "+firstPeople.getLastName()//
-                +", "+"его паспорт "+firstPeople.getPassportId());
-        System.out.println("Второй Человек- это "+secondPeople.getFirstName()+" "+secondPeople.getLastName()//
-                +", "+"его паспорт "+secondPeople.getPassportId());
-        System.out.println("Третий Человек- это "+thirdPeople.getFirstName()+" "+thirdPeople.getLastName()//
-                +", "+"его паспорт "+thirdPeople.getPassportId());
-        System.out.println("Четвертый Человек- это "+fourthPeople.getFirstName()+" "+fourthPeople.getLastName()//
-                +", "+"его паспорт "+fourthPeople.getPassportId());
-    }
     public Person() {
         this.firstName = "";
         this.lastName = "";
@@ -49,6 +30,20 @@ public class Person {
         this.passportId = passportId;
     }
 
+    public Person(String firstName, String lastName, int passportId, Gender gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.passportId = passportId;
+        this.gender = gender;
+    }
+
+    public Person(String firstName, String lastName, int passportId, String gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.passportId = passportId;
+        this.gender = Gender.valueOf(gender);
+    }
+
     public String getFirstName() {
         return firstName;
     }
@@ -71,5 +66,13 @@ public class Person {
 
     public void setPassportId(int passportId) {
         this.passportId = passportId;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
     }
 }
