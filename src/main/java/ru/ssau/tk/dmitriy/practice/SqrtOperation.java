c3b66126ae8f97795dce04b3a92eb0d2450a2d9d
Ostapyuk <dm.ostapyuk@yandex.ru>
2021-09-25 14:57:26 +0400

    Выровнены отступы с помощью известной комбинации клавиш

--- src/main/java/ru/ssau/tk/dmitriy/practice/SqrtOperation.java
+++ src/main/java/ru/ssau/tk/dmitriy/practice/SqrtOperation.java
@@ -1,6 +1,6 @@
 package ru.ssau.tk.dmitriy.practice;
 
-public class SqrtOperation extends Operation{
+public class SqrtOperation extends Operation {
     @Override
     double apply(double number) {
         return Math.sqrt(number);
--- src/main/java/ru/ssau/tk/dmitriy/practice/TangentOperation.java
+++ src/main/java/ru/ssau/tk/dmitriy/practice/TangentOperation.java
@@ -1,6 +1,6 @@
 package ru.ssau.tk.dmitriy.practice;
 
-public class TangentOperation extends Operation{
+public class TangentOperation extends Operation {
     @Override
     double apply(double number) {
         return Math.tan(number);
