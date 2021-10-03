6d7800edac2059941e953ea9a015cb3a568257a2
Ostapyuk <dm.ostapyuk@yandex.ru>
2021-10-03 14:28:12 +0400

    Упражнение 2.7 выполнено согласно заданию, создан массив, заполненный числами, являющимися решениями уравнения по переданным коэф-ам, для метода создан тест

--- src/test/java/ru/ssau/tk/dmitriy/practice/FirstClassForArrayMethodsTest.java
+++ src/test/java/ru/ssau/tk/dmitriy/practice/FirstClassForArrayMethodsTest.java
@@ -57,4 +57,35 @@ public class FirstClassForArrayMethodsTest {
         double[] knownArray = {0.0, 1.0, 4.0, 9.0, 16.0, 25.0, 36.0, 49.0, 64.0, 81.0};
         Assert.assertTrue(Arrays.equals(array, knownArray));
     }
+
+    @Test
+    public void testGetSolvingTheQuadraticEquation() {
+        FirstClassForArrayMethods object = new FirstClassForArrayMethods();
+        double[] firstSolution = object.getSolvingTheQuadraticEquation(0, -5, 2);
+        double[] secondSolution = object.getSolvingTheQuadraticEquation(4, 0, 9);
+        double[] thirdSolution = object.getSolvingTheQuadraticEquation(3, 12, 0);
+        double[] fourthSolution = object.getSolvingTheQuadraticEquation(4, 4, 1);
+        double[] fifthSolution = object.getSolvingTheQuadraticEquation(1, 5, 6);
+        double[] sixthSolution = object.getSolvingTheQuadraticEquation(-1, -5, 14);
+        double[] seventhSolution = object.getSolvingTheQuadraticEquation((double) 1 / 3, (double) -3 / 7, (double) 9 / 5);
+        double[] eighthSolution = object.getSolvingTheQuadraticEquation((double) 1 / 30, (double) -1 / 6, (double) 1 / 5);
+        double[] firstKnownArray = {0.4};
+        double[] secondKnownArray = {}; //0 элементов
+        double[] thirdKnownArray = {-4.0, 0.0};
+        double[] fourthKnownArray = {-0.5};
+        double[] fifthKnownArray = {-3.0, -2.0};
+        double[] sixthKnownArray = {-7.0, 2.0};
+        double[] seventhKnownArray = {}; //0 элементов
+        double[] eighthKnownArray = {2, 3};
+        Assert.assertTrue(Arrays.equals(firstSolution, firstKnownArray));
+        Assert.assertTrue(Arrays.equals(secondSolution, secondKnownArray));
+        Assert.assertTrue(Arrays.equals(thirdSolution, thirdKnownArray));
+        Assert.assertTrue(Arrays.equals(fourthSolution, fourthKnownArray));
+        Assert.assertTrue(Arrays.equals(fifthSolution, fifthKnownArray));
+        Assert.assertTrue(Arrays.equals(sixthSolution, sixthKnownArray));
+        Assert.assertTrue(Arrays.equals(seventhSolution, seventhKnownArray));
+        for (int i = 0; i < eighthSolution.length; i++) {
+            Assert.assertEquals(eighthSolution[i], eighthKnownArray[i], 0.00001);
+        }
+    }
 }
