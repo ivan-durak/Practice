ef585c2348e57b346eb2eff4914d387f87e9be53
Ostapyuk <dm.ostapyuk@yandex.ru>
2021-10-29 20:10:17 +0400

    Упражнение 3.1 выполнено согласно заданию

---
+++ src/main/java/ru/ssau/tk/dmitriy/practice/ClassForStringMethods.java
@@ -0,0 +1,10 @@
+package ru.ssau.tk.dmitriy.practice;
+
+public class ClassForStringMethods {
+
+    public void stringCharactersInTheConsole(String string) {
+        for (int i = 0; i < string.length(); i++) {
+            System.out.println(string.charAt(i));
+        }
+    }
+}
