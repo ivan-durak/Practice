c4b5610a6633977788e87484e06cdd7cae069a23
Ostapyuk <dm.ostapyuk@yandex.ru>
2021-11-06 20:38:15 +0400

    Упражнение 4.2 выполнено согласно заданию, для метода создан соответствующий тест, вроде рассмотрены все виды исключительных ситуаций, о которых было написано

--- src/main/java/ru/ssau/tk/dmitriy/practice/ClassForExceptionMethods.java
+++ src/main/java/ru/ssau/tk/dmitriy/practice/ClassForExceptionMethods.java
@@ -4,4 +4,12 @@ public class ClassForExceptionMethods {
     public String whatKindOfPerson(Person person) {
         return person.toString();
     }
+
+    public char[] arrayOfCharactersFromArrayOfStrings(String[] strings, int index) {
+        char[] chars = new char[strings.length];
+        for (int i = 0; i < strings.length; i++) {
+            chars[i] = strings[i].charAt(index);
+        }
+        return chars;
+    }
 }
--- src/test/java/ru/ssau/tk/dmitriy/practice/ClassForExceptionMethodsTest.java
+++ src/test/java/ru/ssau/tk/dmitriy/practice/ClassForExceptionMethodsTest.java
@@ -14,4 +14,23 @@ public class ClassForExceptionMethodsTest {
             object.whatKindOfPerson(null);
         });
     }
+
+    @Test
+    public void testArrayOfCharactersFromArrayOfStrings() {
+        ClassForExceptionMethods object = new ClassForExceptionMethods();
+        Assert.assertEquals(object.arrayOfCharactersFromArrayOfStrings(new String[]{"descendant", "stream", "kettle", "pedestrian"},//
+                3), new char[]{'c', 'e', 't', 'e'});
+        Assert.assertThrows(NullPointerException.class, () -> {
+            object.arrayOfCharactersFromArrayOfStrings(null, 5);
+        });
+        Assert.assertThrows(NullPointerException.class, () -> {
+            object.arrayOfCharactersFromArrayOfStrings(new String[]{"descendant", "stream", null, "kettle", "pedestrian"}, 5);
+        });
+        Assert.assertThrows(StringIndexOutOfBoundsException.class, () -> {
+            object.arrayOfCharactersFromArrayOfStrings(new String[]{"descendant", "stream", "kettle", "pedestrian"}, -4);
+        });
+        Assert.assertThrows(StringIndexOutOfBoundsException.class, () -> {
+            object.arrayOfCharactersFromArrayOfStrings(new String[]{"descendant", "stream", "kettle", "pedestrian"}, 14);
+        });
+    }
 }
